7eda5752a409e98a224495adaa2b175f42dae2b0
AndersonBatalha <andersonpbatalha@gmail.com>
2022-02-04 19:15:20 -0300

    mudanças na estrutura do html

--- js/index.js
+++ js/index.js
@@ -1,51 +1,37 @@
-const spanHora = document.getElementsByClassName("horas")[0],
-    spanMinuto = document.getElementsByClassName("minutos")[0],
-    spanSegundos = document.getElementsByClassName("segundos")[0],
-    spanDiaDaSemana = document.getElementsByClassName("dia-da-semana")[0],
-    spanDia = document.getElementsByClassName("dia")[0],
-    spanMes = document.getElementsByClassName("mes")[0],
-    spanAno = document.getElementsByClassName("ano")[0];
+const spanHorario = document.querySelector('.horario h1'),
+    spanData = document.querySelector('.data h2');
 
+const MILISSEGUNDOS_PARA_SEGUNDOS = 1000;
 
-const MILISSEGUNDOS_PARA_SEGUNDOS = 1000,
-    MILISSEGUNDOS_PARA_MINUTOS = MILISSEGUNDOS_PARA_SEGUNDOS * 60,
-    MILISSEGUNDOS_PARA_HORAS = MILISSEGUNDOS_PARA_MINUTOS * 60,
-    MILISSEGUNDOS_PARA_DIAS = MILISSEGUNDOS_PARA_HORAS * 24,
-    MILISSEGUNDOS_PARA_MESES = MILISSEGUNDOS_PARA_DIAS * 30,
-    MILISSEGUNDOS_PARA_ANOS = MILISSEGUNDOS_PARA_MESES * 12;
+const diasDaSemana = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"],
+    mesesDoAno = ["janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"];
 
 const functions = {
-    diasDaSemana: ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"],
-    mesesDoAno: ["janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"],
     formataNumero: (valor) => (valor < 10) ? `0${valor}` : `${valor}`,
     alteraValor: (elemento, valor) => {
-        elemento !== undefined && (elemento.innerText = valor);
+        elemento !== null && (elemento.innerText = valor);
     },
-    valorElemento: (elemento) => {
-        return elemento !== undefined ? +elemento.innerText : 0;
+    valorElemento: function(elemento) {
+        return elemento !== null ? elemento.innerText : "";
     },
-    preenche: function(segundo, minuto, hora, diaDaSemana, dia, mes, ano) {
-        functions.alteraValor(spanSegundos, segundo);
-        functions.alteraValor(spanMinuto, minuto);
-        functions.alteraValor(spanHora, hora);
-        functions.alteraValor(spanDiaDaSemana, diaDaSemana);
-        functions.alteraValor(spanDia, dia);
-        functions.alteraValor(spanMes, mes);
-        functions.alteraValor(spanAno, ano);
+    data: function(now) {
+        let horario = now.toLocaleTimeString(),
+            diaDaSemana = now.getDay(),
+            dia = now.getDate(),
+            mes = now.getMonth(),
+            ano = now.getFullYear(),
+            dataCompleta = `${diasDaSemana[diaDaSemana]}, ${this.formataNumero(dia)} de ${mesesDoAno[mes]} de ${ano}`,
+            dataAtual = this.valorElemento(spanData);
+
+        this.alteraValor(spanHorario, horario);
+        dataAtual !== dataCompleta && this.alteraValor(spanData, dataCompleta);
     }
 };
-const now = new Date();
+
+let now = new Date();
+functions.data(now);
 
 setInterval(() => {
-    let now = new Date(),
-        sec = functions.formataNumero(now.getSeconds()),
-        minutes = functions.formataNumero(now.getMinutes()),
-        hour = functions.formataNumero(now.getHours()),
-        dayOfWeek = functions.diasDaSemana[now.getDay()],
-        day = functions.formataNumero(now.getUTCDate()),
-        month = functions.mesesDoAno[now.getMonth()],
-        year = functions.formataNumero(now.getFullYear());
-    console.log(60 - sec, 60 - minutes);
-
-    functions.preenche(sec, minutes, hour, dayOfWeek, day, month, year);
+    let now = new Date();
+    functions.data(now);
 }, MILISSEGUNDOS_PARA_SEGUNDOS);
